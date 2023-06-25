d4ed5ef9e1c739919de53e299890d016c70e6fb9
xmz <pochtanomer3@bk.ru>
2023-06-25 21:27:04 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/Fit.java
@@ -17,7 +17,6 @@ public class Fit {
         double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " + man);
         System.out.println("Woman 187 is " + woman);
-
     }
 
 }
