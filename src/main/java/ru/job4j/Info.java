47c627c6a6a0c8ad193874852f964ee3d2c41722
xmz <pochtanomer3@bk.ru>
2023-06-25 19:42:51 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
-public class Info {
-    public static void main(String[] args) {
-        System.out.println("24.06.2023");
-    }
+        public class Info {
+            @SuppressWarnings("checkstyle:WhitespaceAround")
+            public static void main(String[] args) {
+              byte age = 18;
+              char floor = 'M';
+              float growth = 1.87f;
+              int brick = 35032;
+              double bacterium = 4123142.2312;
+              long distance = 5523523522532L;
+              boolean permission = false;
+              long file = 412412312L;
+            }
 }
